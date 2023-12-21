fef83f2fd198ee9a091073022c0aa447895d7dd2
Mj Saraminia <mjsaraminiadeveloped@gmail.com>
2023-12-21 08:40:34 +0330

    Inputha ro Moratab Kardam va Be Soorate Dynamic Neveshtam

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import styles from '../Components/Contact.module.css'
 import Contactlist from './Contactlist';
+import inputs from '../Constance/Inputs';
 function Contact() {
 
     const [contacts, setContacts] = useState([]);
@@ -11,13 +12,14 @@ function Contact() {
         email: "",
         phone: "",
     });
-
+    //voroodihaye inputha
     const changHandeler = (event) => {
 
         const name = event.target.name;
         const value = event.target.value;
         setContact((contact) => ({ ...contact, [name]: value }))
     };
+
     const addHandeler = () => {
         if (!contact.name || !contact.lastname || !contact.email || !contact.phone) {
             setAlert("Please enter valid data");
@@ -35,29 +37,16 @@ function Contact() {
     return (
         <div>
             <div className={styles.container}>
-                <input type="text"
-                    placeholder='Name'
-                    name='name'
-                    value={contact.name}
-                    onChange={changHandeler} />
-
-                <input type="text"
-                    placeholder='Last Name'
-                    name='lastname'
-                    value={contact.lastname}
-                    onChange={changHandeler} />
-
-                <input type="email"
-                    placeholder='Email'
-                    name='email'
-                    value={contact.email}
-                    onChange={changHandeler} />
-
-                <input type="number"
-                    placeholder='Phone'
-                    name='phone'
-                    value={contact.phone}
-                    onChange={changHandeler} />
+                {/* map mizanam ta inputha ke Moshakhas kardam too jsT sakhte beshan */}
+                {inputs.map((input, index) => (
+                    <input
+                        key={index}
+                        type={input.type}
+                        placeholder={input.placeholder}
+                        name={input.name}
+                        value={contact[input.name]}
+                        onChange={changHandeler}
+                    />))}
 
                 <button onClick={addHandeler} > Add contact</button>
                 <div>{alert && <p>{alert}</p>}</div>
